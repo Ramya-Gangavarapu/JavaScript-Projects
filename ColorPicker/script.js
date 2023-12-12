6b4f6f55462b0350ec7c5eb6b4c5a0b17d34810a
Gangavarapu Ramya <141222323+Ramya-Gangavarapu@users.noreply.github.com>
2023-12-12 19:29:21 +0530

    Add files via upload

---
+++ ColorPicker/script.js
@@ -0,0 +1,22 @@
+let a = document.getElementById("colorPickerContainer");
+let b = document.getElementById("selectedColorHexCode");
+
+function manipulateStylesa() {
+    a.style.backgroundColor = "#e0e0e0";
+    b.textContent = "#e0e0e0";
+}
+
+function manipulateStylesb() {
+    a.style.backgroundColor = "#6fcf97";
+    b.textContent = "#6fcf97";
+}
+
+function manipulateStylesc() {
+    a.style.backgroundColor = "#56ccf2";
+    b.textContent = "#56ccf2";
+}
+
+function manipulateStylesd() {
+    a.style.backgroundColor = "#bb6bd9";
+    b.textContent = "#bb6bd9";
+}
